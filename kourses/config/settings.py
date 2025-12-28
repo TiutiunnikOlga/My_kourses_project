d204535c62f2cd2a05ee98448504092fd36474ca
Olga <tiutiunnikolga@gmail.com>
2025-12-28 19:22:12 +0300

    static_update_4

--- kourses/config/settings.py
+++ kourses/config/settings.py
@@ -107,7 +107,10 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "kourses/static/staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
